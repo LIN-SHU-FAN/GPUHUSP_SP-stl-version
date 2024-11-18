36d45c38d9f98ec3db932b46205b0bdeb9223787
LIN-SHU-FAN <35457943+LIN-SHU-FAN@users.noreply.github.com>
2024-11-18 15:56:35 +0800

    Update main.cu
    2024/11/18 ver

--- main.cu
+++ main.cu
@@ -7,7 +7,9 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <map>
+#include <stack>
 #include <chrono>
+
 using namespace std;
 class DB{
 public:
@@ -20,6 +22,7 @@ public:
     int DButility=0;
 
     unordered_map<int,int> item_swu;
+    vector<int> DB_item_set;
 };
 
 class GPU_DB {
@@ -28,10 +31,25 @@ public:
     int **iu,**ru;
 
     int *sequence_len;
+    int max_sequence_len;
+
     int sid_len;
+
+    vector<int> DB_item_set;
+};
+class Tree_node{
+public:
+    string pattern;
+    int *ProjectArr_first_position,*ProjectArr_len;
+    
 };
 
+
+//int test_max_seq=0;
+
 void parseData(ifstream &file,DB &DBdata) {
+
+
     regex numberRegex("(\\d+)\\[(\\d+)\\]");
 
     vector<int> item,tid;
@@ -64,7 +82,9 @@ void parseData(ifstream &file,DB &DBdata) {
                     }
                 }
 
-
+//                if(item.size()>test_max_seq){
+//                    test_max_seq=item.size();
+//                }
                 DBdata.item.push_back(item);
                 DBdata.iu.push_back(iu);
                 DBdata.ru.push_back(ru);
@@ -101,14 +121,16 @@ void parseData(ifstream &file,DB &DBdata) {
 }
 
 void SWUpruning(double threshold,DB &DBdata){
-    unordered_set<int> ItemSwuUnderThreshold;
-    for(pair<int,int>i:DBdata.item_swu){
-        if(i.second<threshold){
-            ItemSwuUnderThreshold.insert(i.first);
-        }
-    }
-
     DB update_DB;
+
+//    unordered_set<int> ItemSwuUnderThreshold;
+//    for(pair<int,int>i:DBdata.item_swu){
+//        if(i.second<threshold){
+//            ItemSwuUnderThreshold.insert(i.first);
+//        }else{
+//            update_DB.DB_item_set.push_back(i.first);
+//        }
+//    }
     vector<int> item,tid;
     vector<int> iu,ru;
     int seq_len;
@@ -117,7 +139,7 @@ void SWUpruning(double threshold,DB &DBdata){
     for(int i=0;i<sid_len;i++){
         seq_len=0;
         for(int j=0;j<DBdata.sequence_len[i];j++){
-            if(ItemSwuUnderThreshold.find(DBdata.item[i][j])!=ItemSwuUnderThreshold.end()){
+            if(DBdata.item_swu[DBdata.item[i][j]]<threshold){
                 for(int k=0;k<seq_len;k++){
                     ru[k]-=DBdata.iu[i][j];
                 }
@@ -160,8 +182,14 @@ void Bulid_GPU_DB(DB &DBdata,GPU_DB &Gpu_Db){
     Gpu_Db.ru = new int*[Gpu_Db.sid_len];
     Gpu_Db.tid = new int*[Gpu_Db.sid_len];
 
+    int max_sequence_len=0;
+
     for(int i=0;i<Gpu_Db.sid_len;i++){
         Gpu_Db.sequence_len[i] = DBdata.sequence_len[i];
+
+        if(Gpu_Db.sequence_len[i]>max_sequence_len){
+            max_sequence_len=Gpu_Db.sequence_len[i];
+        }
 
 
         Gpu_Db.item[i] = new int[Gpu_Db.sequence_len[i]];
@@ -183,10 +211,41 @@ void Bulid_GPU_DB(DB &DBdata,GPU_DB &Gpu_Db){
 //        cout<<Gpu_Db.sequence_len[i]<<endl;
 
     }
-//    cout<<"";
+    Gpu_Db.max_sequence_len=max_sequence_len;
+
+    Gpu_Db.DB_item_set=DBdata.DB_item_set;
+    //cout<<max_sequence_len;
+}
+
+__global__ void PEUcounter(int project_item,
+                           int** d_item_project, int** d_iu_project, int** d_ru_project, int** d_tid_project,
+                           int* d_sequence_len,int sid_len,
+                           int *d_PEU_seq,int *d_Utility_seq,int *d_project_point) {
+
+    if(threadIdx.x<d_sequence_len[blockIdx.x]){
+        if(d_item_project[blockIdx.x][threadIdx.x]==project_item){
+            atomicMax(&d_PEU_seq[blockIdx.x], d_iu_project[blockIdx.x][threadIdx.x]+d_ru_project[blockIdx.x][threadIdx.x]);
+            atomicMin(&d_project_point[blockIdx.x],int(blockIdx.x));
+            atomicMax(&d_Utility_seq[blockIdx.x], d_iu_project[blockIdx.x][threadIdx.x]);
+        }
+    }
+}
+
+__global__ void PeuCounter(int project_item,
+                           int** d_item_project, int** d_iu_project, int** d_ru_project, int** d_tid_project,
+                           int* d_sequence_len,int sid_len,
+                           int *d_PEU_seq,int *d_project_point) {
+
+    if(threadIdx.x<d_sequence_len[blockIdx.x]){
+        if(d_item_project[blockIdx.x][threadIdx.x]==project_item){
+            atomicMax(&d_PEU_seq[blockIdx.x], d_iu_project[blockIdx.x][threadIdx.x]+d_ru_project[blockIdx.x][threadIdx.x]);
+            atomicMin(&d_project_point[blockIdx.x],int(blockIdx.x));
+        }
+    }
 }
 
 int main() {
+    auto start = std::chrono::high_resolution_clock::now();
 
     // 指定要讀取的檔案名稱
     string filename = "Yoochoose.txt";
@@ -199,24 +258,34 @@ int main() {
         return 1; // 返回錯誤代碼
     }
 
+
     DB DBdata;
     parseData(file,DBdata);
 
+//    cout<<test_max_seq;
     file.close(); // 關閉檔案
 
-    double threshold = 0.000024 * DBdata.DButility;
-
-    auto start = std::chrono::high_resolution_clock::now();
+    double threshold = 0.00014 * DBdata.DButility;
 
     SWUpruning(threshold,DBdata);
-
-
-
 
     GPU_DB Gpu_Db;
 
     Bulid_GPU_DB(DBdata,Gpu_Db);
 
+    
+    stack<Tree_node> Main_stack;
+    Tree_node top1_Node;
+
+    top1_Node.pattern = "";
+    top1_Node.ProjectArr_first_position=new int[Gpu_Db.sid_len];
+    top1_Node.ProjectArr_len=new int[Gpu_Db.sid_len];
+    
+    //DB clearDBdata;
+    //DBdata = clearDBdata;
+    //cout<<Gpu_Db.max_sequence_len;
+    
+    
     size_t freeMem = 0;
     size_t totalMem = 0;
 
@@ -277,7 +346,89 @@ int main() {
     cudaMemcpy(d_tid_project, h_tid_project, Gpu_Db.sid_len * sizeof(int*), cudaMemcpyHostToDevice);
 
 
+
+
+    int *d_sequence_len;
+    cudaMalloc(&d_sequence_len, Gpu_Db.sid_len * sizeof(int));
+    cudaMemcpy(d_sequence_len,Gpu_Db.sequence_len,Gpu_Db.sid_len * sizeof(int),cudaMemcpyHostToDevice);
+//    for(int i=0;i<Gpu_Db.sid_len;i++){
+//        cout<<Gpu_Db.sequence_len[i]<<endl;
+//    }
+//    cout<<endl;
     //kernelfunction操作
+
+    //如果sequence長度有超過1024需要額外處理
+    //int *block_per_sequence = new int[Gpu_Db.sid_len];
+//    if(Gpu_Db.max_sequence_len>1024){
+//        threadsPerBlock=1024;
+//        for(int i=0;i<Gpu_Db.sid_len;i++){
+//            blocksPerGrid+= (Gpu_Db.sequence_len[i]+threadsPerBlock-1)/threadsPerBlock;
+//            block_per_sequence[i] = (Gpu_Db.sequence_len[i]+threadsPerBlock-1)/threadsPerBlock;
+//        }
+//    }else{
+//        threadsPerBlock=Gpu_Db.max_sequence_len;
+//        blocksPerGrid=Gpu_Db.sid_len;
+//    }
+
+    int *d_PEU_seq;
+    cudaMalloc(&d_PEU_seq, Gpu_Db.sid_len * sizeof(int));
+    cudaMemset(d_PEU_seq, 0, Gpu_Db.sid_len * sizeof(int));
+
+    int *d_Utility_seq;
+    cudaMalloc(&d_Utility_seq, Gpu_Db.sid_len * sizeof(int));
+    cudaMemset(d_Utility_seq, 0, Gpu_Db.sid_len * sizeof(int));
+
+    int *d_project_point;
+    cudaMalloc(&d_project_point, Gpu_Db.sid_len * sizeof(int));
+
+    int threadsPerBlock;
+    int blocksPerGrid;
+
+    for(int i:Gpu_Db.DB_item_set){
+        threadsPerBlock=Gpu_Db.max_sequence_len;
+        blocksPerGrid=Gpu_Db.sid_len;
+//        for(int j=0;j<Gpu_Db.sid_len;j++){
+//
+//        }
+
+        //這裡之後改成依照每個sqe長度跑thread
+        PEUcounter<<<blocksPerGrid,threadsPerBlock>>>(i,d_item_project,d_iu_project,d_ru_project,d_tid_project
+                                                      ,d_sequence_len,Gpu_Db.sid_len,d_PEU_seq,d_Utility_seq,d_project_point);
+
+        int *h_PEU_seq=new int[Gpu_Db.sid_len];
+        cudaMemcpy(h_PEU_seq, d_PEU_seq, Gpu_Db.sid_len*sizeof(int), cudaMemcpyDeviceToHost);
+
+        int *h_Utility_seq=new int[Gpu_Db.sid_len];
+        cudaMemcpy(h_Utility_seq, d_Utility_seq, Gpu_Db.sid_len*sizeof(int), cudaMemcpyDeviceToHost);
+        
+        //這裡可以用加總
+        int PEU_count=0;
+        for(int j=0;j<Gpu_Db.sid_len;j++){
+            PEU_count+=h_PEU_seq[j];
+            if(PEU_count>=threshold){
+                break;
+            }
+            //cout<<h_PEU_seq[j]<<endl;
+        }
+
+        if(PEU_count<threshold){
+            continue;
+        }
+
+        //cout<<i<<endl;
+
+        cudaMemset(d_PEU_seq, 0, Gpu_Db.sid_len * sizeof(int));
+        //cout<<"*/*****\n";
+    }
+
+//    PEUcounter<<<blocksPerGrid,threadsPerBlock>>>(821024,d_item_project,d_iu_project,d_ru_project,d_tid_project,d_sequence_len,Gpu_Db.sid_len,d_PEU_seq);
+//
+//    int *h_Result=new int[Gpu_Db.sid_len];
+//    cudaMemcpy(h_Result, d_PEU_seq, Gpu_Db.sid_len*sizeof(int), cudaMemcpyDeviceToHost);
+//
+//    for(int j=0;j<Gpu_Db.sid_len;j++){
+//        cout<<h_Result[j]<<endl;
+//    }
 
 
     // 獲取 GPU 的內存信息
@@ -301,12 +452,15 @@ int main() {
     delete[] h_iu_project;
     delete[] h_ru_project;
     delete[] h_tid_project;
+    //delete[] h_Result;
 
     cudaFree(d_item_project);
     cudaFree(d_iu_project);
     cudaFree(d_ru_project);
     cudaFree(d_tid_project);
 
+    cudaFree(d_sequence_len);
+    cudaFree(d_PEU_seq);
 
 //    // 獲取 GPU 的內存信息
 //    status = cudaMemGetInfo(&freeMem, &totalMem);
